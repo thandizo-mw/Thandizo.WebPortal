7582c92296dd30a4be913ab87cd1af9f7d076a13
Vitumbiko Vinkhumbo <vitumbiko.vinkhumbo@angledimension.com>
2020-04-06 15:05:50 +0200

    Initial Commit

---
+++ Thandizo.WebPortal/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Thandizo.WebPortal.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
