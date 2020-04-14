b7f82d8be6e087fbb6208d7fea167480f7cbd907
Vitumbiko Vinkhumbo <vitumbiko.vinkhumbo@angledimension.com>
2020-04-14 23:59:13 +0200

    Added OutputHandler helper class

---
+++ Thandizo.WebPortal/Helpers/OutputHandler.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Thandizo.WebPortal.Helpers
+{
+    public class OutputHandler
+    {
+        //public OutputHandler();
+
+        public bool IsErrorOccured { get; set; }
+        public string Message { get; set; }
+        public object Result { get; set; }
+        public string ResponseCode { get; set; }
+    }
+}
