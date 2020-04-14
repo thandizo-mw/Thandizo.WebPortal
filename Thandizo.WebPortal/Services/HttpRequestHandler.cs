37b87e2bf1ccac7e67d3a5e5c7822598f2241613
Vitumbiko Vinkhumbo <vitumbiko.vinkhumbo@angledimension.com>
2020-04-15 00:00:05 +0200

    Added HttpRequestHandler and IHttpRequestHandler service classes

---
+++ Thandizo.WebPortal/Services/HttpRequestHandler.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace Thandizo.WebPortal.Services
+{
+    public class HttpRequestHandler:IHttpRequestHandler
+    {
+        public async Task<HttpResponseMessage> Get(string url)
+        {
+            var client = new HttpClient();
+            var request = new HttpRequestMessage
+            {
+                Method = new HttpMethod("Get"),
+                RequestUri = new Uri(url),
+            };
+            request.Headers.Accept.Clear();
+            return await client.SendAsync(request);
+        }
+    }
+}
---
+++ Thandizo.WebPortal/Services/IHttpRequestHandler.cs
@@ -0,0 +1,10 @@
+﻿using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace Thandizo.WebPortal.Services
+{
+    public interface IHttpRequestHandler
+    {
+        Task<HttpResponseMessage> Get(string url);
+    }
+}
