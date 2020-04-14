b3e614e85b44db60da846086b1d6d2488c2fda27
Vitumbiko Vinkhumbo <vitumbiko.vinkhumbo@angledimension.com>
2020-04-15 00:04:14 +0200

    Added Domain service registration

--- Thandizo.WebPortal/Startup.cs
+++ Thandizo.WebPortal/Startup.cs
@@ -25,6 +25,7 @@ namespace Thandizo.WebPortal
         {
             services.AddControllersWithViews()
                 .AddRazorRuntimeCompilation();
+            services.AddDomainServices();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
