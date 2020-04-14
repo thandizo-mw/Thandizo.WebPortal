747068b44b9a9e7627f679c8a966d4622ff87499
Vitumbiko Vinkhumbo <vitumbiko.vinkhumbo@angledimension.com>
2020-04-14 23:58:34 +0200

    Added PatientController Class

---
+++ Thandizo.WebPortal/Controllers/PatientsController.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using Thandizo.DataModels.Patients;
+using Thandizo.WebPortal.Services;
+
+namespace Thandizo.WebPortal.Controllers
+{
+    public class PatientsController : Controller
+    {
+        private readonly IConfiguration _configuration;
+        IHttpRequestHandler _httpRequestHandler;
+
+        public PatientsController(IConfiguration configuration, IHttpRequestHandler httpRequestHandler)
+        {
+            _configuration = configuration;
+            _httpRequestHandler = httpRequestHandler;
+        }
+
+        public string PatientsApiUrl
+        {
+            get
+            {
+                return _configuration["PatientsApiUrl"];
+            }
+        }
+        
+        public async Task<IActionResult> IndexAsync()
+        {
+            string url = $"{PatientsApiUrl}Patients/GetAll";
+            var patients = Enumerable.Empty<PatientDTO>();
+
+            var response = await _httpRequestHandler.Get(url);
+          
+            if (response.StatusCode == HttpStatusCode.OK)
+            {
+                patients = response.ContentAsType<IEnumerable<PatientDTO>>();
+            }
+            else 
+            {
+                ModelState.AddModelError("", HttpResponseHandler.Process(response));
+                
+            }
+                       
+            return View(patients);
+        }        
+
+        public async Task<IActionResult> ConfirmPatient([FromQuery] long pid)
+        {
+            string url = $"{PatientsApiUrl}/api/Patients/GetById?patientId={pid}";
+            var patient = new PatientDTO();
+
+            var response = await _httpRequestHandler.Get(url);
+
+            if (response.StatusCode == HttpStatusCode.OK)
+            {
+                patient = response.ContentAsType<PatientDTO>();
+            }
+            else
+            {
+                ModelState.AddModelError("", HttpResponseHandler.Process(response));
+
+            }
+            return View(patient);
+        }
+    }
+}
