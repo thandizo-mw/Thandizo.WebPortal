cedbcdd6eba3298a75bef2fdfa34592c1936c070
Vitumbiko Vinkhumbo <vitumbiko.vinkhumbo@angledimension.com>
2020-04-15 16:40:01 +0200

    Added JsonContent helper class

---
+++ Thandizo.WebPortal/Helpers/JsonContent.cs
@@ -0,0 +1,24 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Thandizo.WebPortal.Helpers
+{
+    public class JsonContent : StringContent
+    {
+        public JsonContent(object value)
+            : base(JsonConvert.SerializeObject(value), Encoding.UTF8,
+            "application/json")
+        {
+        }
+
+        public JsonContent(object value, string mediaType)
+            : base(JsonConvert.SerializeObject(value), Encoding.UTF8, mediaType)
+        {
+        }
+    }
+}
