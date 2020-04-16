ccc89809e9a6d679f463bd2b1c140d6ef4918ad5
Vitumbiko Vinkhumbo <vitumbiko.vinkhumbo@angledimension.com>
2020-04-16 16:50:48 +0200

    Added MessageType class in helpers

---
+++ Thandizo.WebPortal/Helpers/General/MessageType.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Thandizo.WebPortal.Helpers.General
+{
+    public enum MessageType
+    {
+        Success = 0,
+        Information = 1,
+        Warning = 2,
+        Danger = 3,
+        CriticalDanger = 4,
+        Alternative = 5
+    }
+}
