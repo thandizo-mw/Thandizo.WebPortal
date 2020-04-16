54a0e0cba39842c8106c7e4398b6b8606686a711
Vitumbiko Vinkhumbo <vitumbiko.vinkhumbo@angledimension.com>
2020-04-16 08:51:45 +0200

    Added data ble definitions in dataTable.js files

--- Thandizo.WebPortal/wwwroot/js/dataTables-data.js
+++ Thandizo.WebPortal/wwwroot/js/dataTables-data.js
@@ -3,6 +3,32 @@
 $(document).ready(function() {
 	"use strict";
 	
-	$('#datable_1').DataTable();
-    $('#datable_2').DataTable({ "lengthChange": false});
-} );
+    $('#datable_1').DataTable();
+
+    $('#datable_2').DataTable({ "lengthChange": false });
+
+    $('#dtDataTable').DataTable({
+        "lengthMenu": [6, 8]
+    });
+
+    $('#dtScrollableDynamicTable').DataTable({
+        scrollY: '45vh',
+        scrollCollapse: true,
+        paging: false
+    });
+
+    $('#dtScrollableFixedTable').DataTable({
+        scrollY: '200px',
+        scrollCollapse: true,
+        paging: false
+    });
+
+    $('#dtScrollableNoDefaults').DataTable({
+        "searching": false,
+        "lengthMenu": [6, 8],
+        "ordering": false,
+        scrollY: '45vh',
+        scrollCollapse: true,
+        paging: false
+    });
+});
