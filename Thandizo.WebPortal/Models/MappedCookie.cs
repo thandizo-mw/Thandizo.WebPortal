95411c72bbb221b202ab625f707cf16f3465e5f5
Vitumbiko Vinkhumbo <vitumbiko.vinkhumbo@angledimension.com>
2020-04-18 09:23:03 +0200

    Added MappedCookies model class

---
+++ Thandizo.WebPortal/Models/MappedCookie.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Thandizo.WebPortal.Models
+{
+    public class MappedCookie
+    {
+        public string FriendlyName { get; set; }
+        public string CookieKey { get; set; }
+    }
+}
