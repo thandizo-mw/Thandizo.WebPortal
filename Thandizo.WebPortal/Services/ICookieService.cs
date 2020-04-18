e1b17077ad09c2aed2868626e79bd69c42fe8c97
Vitumbiko Vinkhumbo <vitumbiko.vinkhumbo@angledimension.com>
2020-04-18 09:22:44 +0200

    Added CookieService and ICookieService classes

---
+++ Thandizo.WebPortal/Services/ICookieService.cs
@@ -0,0 +1,9 @@
+﻿namespace Thandizo.WebPortal.Services
+{
+    public interface ICookieService
+    {
+        void Add(string friendlyName, string value);
+        void Delete(string friendlyName);
+        string Get(string friendlyName);
+    }
+}
