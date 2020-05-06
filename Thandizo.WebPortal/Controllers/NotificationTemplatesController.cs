f28004c06a6747870be5d1b5a84b7f09d4753c78
Vitumbiko Vinkhumbo <vitumbiko.vinkhumbo@angledimension.com>
2020-05-06 10:14:44 +0200

    Removed unsed usings in NotificationsTemplatesController class

--- Thandizo.WebPortal/Controllers/NotificationTemplatesController.cs
+++ Thandizo.WebPortal/Controllers/NotificationTemplatesController.cs
@@ -7,7 +7,6 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Net;
 using System.Threading.Tasks;
-using Thandizo.DataModels.Core;
 using Thandizo.DataModels.Notifications;
 using Thandizo.WebPortal.Filters;
 using Thandizo.WebPortal.Helpers;
