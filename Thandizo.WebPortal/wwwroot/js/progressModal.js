defc961e49794a0b607d2bc23a2f2ad53ae7f8cd
Vitumbiko Vinkhumbo <vitumbiko.vinkhumbo@angledimension.com>
2020-05-20 16:31:40 +0200

    Added progressModal.js file

---
+++ Thandizo.WebPortal/wwwroot/js/progressModal.js
@@ -0,0 +1,11 @@
+﻿$(document).ready(function () {
+    $('#progressModal').modal('hide');
+
+    $("form").submit(function (e) {
+        if ($(this).hasClass("hide-progress-window")) {
+            $('#progressModal').modal('hide');
+        } else {
+            $('#progressModal').modal('show');
+        }
+    });
+});
